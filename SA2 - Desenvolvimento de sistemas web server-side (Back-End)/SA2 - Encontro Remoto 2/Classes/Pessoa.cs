7469bfbb6e9a077c92da8a28f944262f3f3886bd
palomaedeltrudes <lomita.trudys@gmail.com>
2022-09-07 21:44:45 -0300

    add atividade cod back-end

---
+++ SA2 - Desenvolvimento de sistemas web server-side (Back-End)/SA2 - Encontro Remoto 2/Classes/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SA2___Desenvolvimento_de_sistemas_web_server_side__Back_End_.SA2 - Encontro Remoto 2.Classes
+{
+    public class Pessoa
+    {
+        
+    }
+}
